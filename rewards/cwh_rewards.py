24cdc58861a3c3f941470549af0222451b5f1eb2
Umberto Ravaioli <uravaioli@toyon.com>
2021-10-07 10:05:08 -0700

    cwh distance change reward validator added

--- rewards/cwh_rewards.py
+++ rewards/cwh_rewards.py
@@ -6,16 +6,21 @@ from pydantic import BaseModel
 from act3_rl_core.libraries.state_dict import StateDict
 from act3_rl_core.libraries.env_func_base import EnvFuncBase
 from act3_rl_core.libraries.environment_dict import RewardDict
-from act3_rl_core.rewards.reward_func_base import RewardFuncBase
+from act3_rl_core.rewards.reward_func_base import RewardFuncBase, RewardFuncBaseValidator
 
 
+class CWHDistanceChangeRewardValidator(RewardFuncBaseValidator):
+    scale : float
 
 class CWHDistanceChangeReward(RewardFuncBase):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._dist_buffer = RingBuffer(capacity=2, dtype=float)
-        self.scale = -1.0e-05
+
+    @classmethod
+    def get_validator(cls):
+        return CWHDistanceChangeRewardValidator
 
     def __call__(
         self,
@@ -38,7 +43,7 @@ class CWHDistanceChangeReward(RewardFuncBase):
 
         # TODO intialize distance buffer from initial state
         if len(self._dist_buffer) == 2:
-            val = self.scale * (self._dist_buffer[1] - self._dist_buffer[0])
+            val = self.config.scale * (self._dist_buffer[1] - self._dist_buffer[0])
 
         reward[self.config.agent_name] = val
 
