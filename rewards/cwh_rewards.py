cc165730e73acf44f7450b065d1748d05ff7f86c
james.cunningham <james.cunningham@jacobs.com>
2021-10-07 13:24:54 -0400

    Reward test

--- rewards/cwh_rewards.py
+++ rewards/cwh_rewards.py
@@ -10,7 +10,8 @@ from act3_rl_core.rewards.reward_func_base import RewardFuncBase, RewardFuncBase
 
 
 class CWHDistanceChangeRewardValidator(RewardFuncBaseValidator):
-    scale : float
+    scale: float
+
 
 class CWHDistanceChangeReward(RewardFuncBase):
 
@@ -50,3 +51,67 @@ class CWHDistanceChangeReward(RewardFuncBase):
         return reward
 
 
+if __name__ == "__main__":
+    from collections import OrderedDict
+    from simulators.cwh_simulator import CWHSimulator
+
+    tmp_config = {
+        "step_size": 1,
+        "agent_configs": {
+            "blue0": {
+                "sim_config": {
+                },
+                "platform_config": [
+                    (
+                        "space.cwh.platforms.cwh_controllers.ThrustController",
+                        {"name": "X Thrust", "axis": 0}
+                    ),
+                    (
+                        "space.cwh.platforms.cwh_controllers.ThrustController",
+                        {"name": "Y Thrust", "axis": 1}
+                    ),
+                    (
+                        "space.cwh.platforms.cwh_controllers.ThrustController",
+                        {"name": "Z Thrust", "axis": 2}
+                    ),
+                    (
+                        "space.cwh.platforms.cwh_sensors.PositionSensor",
+                        {}
+                    ),
+                    (
+                        "space.cwh.platforms.cwh_sensors.VelocitySensor",
+                        {}
+                    )
+                ]
+            }
+        }
+    }
+
+    reset_config = {
+        "agent_initialization": {
+            "blue0": {
+                "position": [1, 1, 1],
+                "velocity": [-0.5, -0.5, -0.5]
+            }
+        }
+    }
+
+    tmp = CWHSimulator(**tmp_config)
+    state = tmp.reset(reset_config)
+
+    reward_fn = CWHDistanceChangeReward(agent_name="blue0", scale=-1e-2)
+
+    for i in range(5):
+        state = tmp.step()
+        reward = reward_fn(
+            observation=OrderedDict(),
+            action=OrderedDict(),
+            next_observation=OrderedDict(),
+            state=StateDict(),
+            next_state=state,
+            observation_space=StateDict(),
+            observation_units=StateDict()
+        )
+        print("Position: %s\t Velocity: %s" % (
+            str(state.sim_platforms[0].position), str(state.sim_platforms[0].velocity)))
+        print(reward)
