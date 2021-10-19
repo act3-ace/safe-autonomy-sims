b0727d652266b6596f372e5d3302c0e63f8427fe
james.cunningham <james.cunningham@jacobs.com>
2021-10-19 18:59:44 -0400

    Dubins 2d and 3d sim initial implementation

--- saferl/simulators/dubins/dubins_simulator.py
+++ saferl/simulators/dubins/dubins_simulator.py
@@ -1,73 +1,41 @@
 import typing
 
 from act3_rl_core.libraries.plugin_library import PluginLibrary
-from act3_rl_core.libraries.state_dict import StateDict
-from act3_rl_core.simulators.base_simulator import BaseSimulator, BaseSimulatorResetValidator, BaseSimulatorValidator
+from act3_rl_core.simulators.base_simulator import BaseSimulatorResetValidator
 from pydantic import BaseModel, validator
 
-from saferl.platforms.dubins.dubins_platform import Dubins2dPlatform
-import saferl.simulators.dubins.backend.platforms as b
+from saferl.platforms.dubins.dubins_platform import Dubins2dPlatform, Dubins3dPlatform
+import saferl.simulators.dubins.backend.platforms as bp
+from saferl.simulators.saferl_simulator import SafeRLSimulator
 
 
-class DubinsSimulatorValidator(BaseSimulatorValidator):
-    step_size: float
-
-
-class DubinsPlatformConfigValidator(BaseModel):
+class Dubins2dPlatformConfigValidator(BaseModel):
     position: typing.List[float]
-    velocity: typing.List[float]
+    speed: float
     heading: float
 
-    @validator("position", "velocity")
+    @validator("position")
     def check_position_len(cls, v, field):
-        if len(v) != 3:
-            raise ValueError(f"{field.name} provided to DubinsPlatformConfigValidator is not length 3")
+        check_len = 2
+        if len(v) != check_len:
+            raise ValueError(f"{field.name} provided to DubinsPlatformConfigValidator is not length {check_len}")
         return v
 
 
-class DubinsSimulatorResetValidator(BaseSimulatorResetValidator):
-    agent_initialization: typing.Optional[typing.Dict[str, DubinsPlatformConfigValidator]] = {
-        "blue0": DubinsPlatformConfigValidator(position=[0, 1, 2], velocity=[0, 0, 0])
+class Dubins2dSimulatorResetValidator(BaseSimulatorResetValidator):
+    agent_initialization: typing.Optional[typing.Dict[str, Dubins2dPlatformConfigValidator]] = {
+        "blue0": Dubins2dPlatformConfigValidator(position=[0, 1], speed=100, heading=0)
     }
 
 
-# TODO: Create combined CWH/Dubins saferl simulator superclass
-
-class Dubins2dSimulator(BaseSimulator):
-
-    @classmethod
-    def get_simulator_validator(cls):
-        return DubinsSimulatorValidator
+class Dubins2dSimulator(SafeRLSimulator):
 
     @classmethod
     def get_reset_validator(cls):
-        return DubinsSimulatorResetValidator
+        return Dubins2dSimulatorResetValidator
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.sim_entities = {agent_id: b.Dubins2dPlatform(name=agent_id) for agent_id in self.config.agent_configs.keys()}
-        self._state = StateDict()
-        self.clock = 0.0
-
-    def reset(self, config):
-        self._state.clear()
-        self.clock = 0.0
-        config = self.get_reset_validator()(**config)
-        for agent_id, entity in self.sim_entities.items():
-            i = config.agent_initialization[agent_id]
-            self.sim_entities[agent_id].reset(
-                **{
-                    "x": i.position[0],
-                    "y": i.position[1],
-                    "z": i.position[2],
-                    "x_dot": i.velocity[0],
-                    "y_dot": i.velocity[1],
-                    "z_dot": i.velocity[2]
-                }
-            )
-        self._state.sim_platforms = self.get_platforms()
-        self.update_sensor_measurements()
-        return self._state
+    def get_sim_entities(self):
+        return {agent_id: bp.Dubins2dPlatform(name=agent_id) for agent_id in self.config.agent_configs.keys()}
 
     def get_platforms(self):
         sim_platforms = tuple(
@@ -75,65 +43,144 @@ class Dubins2dSimulator(BaseSimulator):
         )
         return sim_platforms
 
-    def update_sensor_measurements(self):
-        """
-        Update and caches all the measurements of all the sensors on each platform
-        """
-        for plat in self._state.sim_platforms:
-            for sensor in plat.sensors:
-                sensor.calculate_and_cache_measurement(state=self._state.sim_platforms)
-
-    def mark_episode_done(self):
-        pass
-
-    def save_episode_information(self, **kwargs):
-        pass
-
-    def step(self):
-        for platform in self._state.sim_platforms:
-            agent_id = platform.name
-            import numpy as np
-            action = np.array(platform.get_applied_action(), dtype=np.float32)
-            entity = self.sim_entities[agent_id]
-            entity.step_compute(sim_state=None, action=action, step_size=self.config.step_size)
-            entity.step_apply()
-            platform.sim_time = self.clock
-        self.update_sensor_measurements()
-        self.clock += self.config.step_size
-        return self._state
+    def reset_sim_entities(self, config):
+        config = self.get_reset_validator()(**config)
+        for agent_id, entity in self.sim_entities.items():
+            init_params = config.agent_initialization[agent_id]
+            self.sim_entities[agent_id].reset(
+                **{
+                    "x": init_params.position[0],
+                    "y": init_params.position[1],
+                    "heading": init_params.heading,
+                    "v": init_params.speed
+                }
+            )
 
 
-PluginLibrary.AddClassToGroup(Dubins2dSimulator, "Dubins2dSimulator", {})
+class Dubins3dPlatformConfigValidator(BaseModel):
+    position: typing.List[float]
+    speed: float
+    heading: float
+    gamma: float
+    roll: float
+
+    @validator("position")
+    def check_position_len(cls, v, field):
+        check_len = 3
+        if len(v) != check_len:
+            raise ValueError(f"{field.name} provided to Dubins3dPlatformConfigValidator is not length {check_len}")
+        return v
+
+
+class Dubins3dSimulatorResetValidator(BaseSimulatorResetValidator):
+    agent_initialization: typing.Optional[typing.Dict[str, Dubins3dPlatformConfigValidator]] = {
+        "blue0": Dubins3dPlatformConfigValidator(position=[0, 1, 2], speed=100, heading=0, gamma=0, roll=0)
+    }
+
+
+class Dubins3dSimulator(SafeRLSimulator):
+
+    @classmethod
+    def get_reset_validator(cls):
+        return Dubins3dSimulatorResetValidator
+
+    def get_sim_entities(self):
+        return {agent_id: bp.Dubins3dPlatform(name=agent_id) for agent_id in self.config.agent_configs.keys()}
+
+    def get_platforms(self):
+        sim_platforms = tuple(
+            Dubins3dPlatform(platform_name=agent_id, platform=entity, platform_config=self.config.agent_configs[agent_id].platform_config) for agent_id, entity in self.sim_entities.items()
+        )
+        return sim_platforms
+
+    def reset_sim_entities(self, config):
+        config = self.get_reset_validator()(**config)
+        for agent_id, entity in self.sim_entities.items():
+            init_params = config.agent_initialization[agent_id]
+            self.sim_entities[agent_id].reset(
+                **{
+                    "x": init_params.position[0],
+                    "y": init_params.position[1],
+                    "z": init_params.position[2],
+                    "heading": init_params.heading,
+                    "v": init_params.speed,
+                    "gamma": init_params.gamma,
+                    "roll": init_params.roll
+                }
+            )
+
+
+PluginLibrary.AddClassToGroup(Dubins3dSimulator, "Dubins3dSimulator", {})
 
 
 if __name__ == "__main__":
-    tmp_config = {
+    tmp_config_2d = {
         "step_size": 1,
         "agent_configs": {
             "blue0": {
                 "sim_config": {},
                 "platform_config": [
-                    ("saferl.platforms.cwh.cwh_controllers.ThrustController", {
-                        "name": "X Thrust", "axis": 0
-                    }), ("saferl.platforms.cwh.cwh_controllers.ThrustController", {
-                        "name": "Y Thrust", "axis": 1
-                    }), ("saferl.platforms.cwh.cwh_controllers.ThrustController", {
-                        "name": "Z Thrust", "axis": 2
-                    }), ("saferl.platforms.cwh.cwh_sensors.PositionSensor", {}), ("saferl.platforms.cwh.cwh_sensors.VelocitySensor", {})
+                    # ("saferl.platforms.dubins.dubins_controllers.CombinedTurnRateAccelerationController", {
+                    #     "name": "YawAccControl"
+                    # }),
+                    ("saferl.platforms.dubins.dubins_controllers.YawRateController", {
+                        "name": "YawRateControl", "axis": 0
+                    }),
+                    ("saferl.platforms.dubins.dubins_controllers.AccelerationController", {
+                        "name": "AccelerationControl", "axis": 1
+                    }),
+                    ("saferl.platforms.dubins.dubins_sensors.PositionSensor", {}),
+                    ("saferl.platforms.dubins.dubins_sensors.VelocitySensor", {}),
+                    ("saferl.platforms.dubins.dubins_sensors.HeadingSensor", {})
                 ]
             }
         }
     }
 
-    reset_config = {"agent_initialization": {"blue0": {"position": [0, 1, 2], "velocity": [0, 0, 0]}}}
+    tmp_config_3d = {
+        "step_size": 1,
+        "agent_configs": {
+            "blue0": {
+                "sim_config": {},
+                "platform_config": [
+                    ("saferl.platforms.dubins.dubins_controllers.CombinedPitchRollAccelerationController", {
+                        "name": "PitchRollAccControl"
+                    }),
+                    # ("saferl.platforms.dubins.dubins_controllers.PitchRateController", {
+                    #     "name": "PitchRateControl", "axis": 0
+                    # }),
+                    # ("saferl.platforms.dubins.dubins_controllers.RollRateController", {
+                    #     "name": "RollRateControl", "axis": 1
+                    # }),
+                    # ("saferl.platforms.dubins.dubins_controllers.AccelerationController", {
+                    #     "name": "AccelerationControl", "axis": 2
+                    # }),
+                    ("saferl.platforms.dubins.dubins_sensors.PositionSensor", {}),
+                    ("saferl.platforms.dubins.dubins_sensors.VelocitySensor", {}),
+                    ("saferl.platforms.dubins.dubins_sensors.HeadingSensor", {}),
+                    # ("saferl.platforms.dubins.dubins_sensors.FlightPathSensor", {}),
+                ]
+            }
+        }
+    }
 
-    tmp = Dubins2dSimulator(**tmp_config)
+
+    # reset_config = {"agent_initialization": {"blue0": {"position": [0, 1], "heading": 0, "speed": 50}}}
+    reset_config = {"agent_initialization": {
+        "blue0": {"position": [0, 1, 2], "heading": 0, "speed": 50, "gamma": 0, "roll": 0}}}
+
+    # tmp = Dubins2dSimulator(**tmp_config_2d)
+    tmp = Dubins3dSimulator(**tmp_config_3d)
+
     state = tmp.reset(reset_config)
-    # print("Position: %s\t Velocity: %s" % (str(state.sim_platforms[0].position), str(state.sim_platforms[0].velocity)))
+    print(
+        f"Position: {state.sim_platforms[0].position}\t Velocity: {state.sim_platforms[0].velocity}\tHeading: {state.sim_platforms[0].heading}")
     for i in range(5):
-        state.sim_platforms[0]._controllers[0].apply_control(1)
-        # state.sim_platforms[0]._controllers[1].apply_control(2)
-        # state.sim_platforms[0]._controllers[2].apply_control(3)
+        control = [1, 0, 0]
+        state.sim_platforms[0]._controllers[0].apply_control(control)
+        # state.sim_platforms[0]._controllers[0].apply_control(control[0])
+        # state.sim_platforms[0]._controllers[1].apply_control(control[1])
+        # state.sim_platforms[0]._controllers[2].apply_control(control[2])
         # print(state.sim_platforms[0]._sensors[1].get_measurement())
         state = tmp.step()
-        print("Position: %s\t Velocity: %s" % (str(state.sim_platforms[0].position), str(state.sim_platforms[0].velocity)))
+        print(f"Position: {state.sim_platforms[0].position}\t Velocity: {state.sim_platforms[0].velocity}\tHeading: {state.sim_platforms[0].heading}")
