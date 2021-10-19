8aa6357770b6013e43fe8980d085f77ca04d6e46
james.cunningham <james.cunningham@jacobs.com>
2021-10-19 17:26:39 -0400

    Initial Dubins 2D platform implementation

--- saferl/platforms/dubins/dubins_sensors.py
+++ saferl/platforms/dubins/dubins_sensors.py
@@ -3,6 +3,11 @@ import math
 from act3_rl_core.libraries.property import MultiBoxProp
 from act3_rl_core.simulators.base_parts import BaseSensor
 
+from act3_rl_core.libraries.plugin_library import PluginLibrary
+
+from saferl.simulators.dubins.dubins_simulator import Dubins2dSimulator
+from saferl.platforms.dubins.dubins_available_platforms import DubinsAvailablePlatformTypes
+
 
 class DubinsSensor(BaseSensor):
 
@@ -29,6 +34,13 @@ class PositionSensor(DubinsSensor):
         return self.parent_platform.position
 
 
+PluginLibrary.AddClassToGroup(
+    PositionSensor, "Sensor_Position", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+
+
 class VelocitySensor(DubinsSensor):
 
     def measurement_properties(self):
@@ -40,6 +52,54 @@ class VelocitySensor(DubinsSensor):
     def _calculate_measurement(self, state):
         return self.parent_platform.velocity
 
+
+PluginLibrary.AddClassToGroup(
+    VelocitySensor, "Sensor_Velocity", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+
+
+class HeadingSensor(DubinsSensor):
+
+    def measurement_properties(self):
+        heading_properties = MultiBoxProp(
+            name="heading", low=[-2*math.pi], high=[2*math.pi], unit=["rad"], description="heading of the aircraft"
+        )
+        return heading_properties
+
+    def _calculate_measurement(self, state):
+        return self.parent_platform.heading
+
+
+PluginLibrary.AddClassToGroup(
+    HeadingSensor, "Sensor_Heading", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+
+
+class FlightPathSensor(DubinsSensor):
+
+    def measurement_properties(self):
+        fp_properties = MultiBoxProp(
+            name="flight_path_angle", low=[-2*math.pi], high=[2*math.pi], unit=["rad"],
+            description="flight path angle of the aircraft"
+        )
+        return fp_properties
+
+    def _calculate_measurement(self, state):
+        return self.parent_platform.gamma
+
+
+# PluginLibrary.AddClassToGroup(
+#     FlightPathSensor, "Sensor_Flight_Path_Angle", {
+#         "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS3D
+#     }
+# )
+
+
+# TODO: Add to plugin group
 
 class TimeSensor(DubinsSensor):
 
