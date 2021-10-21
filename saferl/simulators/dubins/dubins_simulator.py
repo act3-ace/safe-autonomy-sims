9695f52063af3af0cbb16e2affd1ed4e9952b210
vgangal <Vardaan.Gangal@Jacobs.com>
2021-10-21 15:46:28 -0400

    docs for dubins simulator

--- saferl/simulators/dubins/dubins_simulator.py
+++ saferl/simulators/dubins/dubins_simulator.py
@@ -1,3 +1,6 @@
+"""
+Contains the implementations of classes that describe how the simulation is to proceed.
+"""
 import typing
 
 from act3_rl_core.libraries.plugin_library import PluginLibrary
@@ -10,12 +13,24 @@ from saferl.simulators.saferl_simulator import SafeRLSimulator
 
 
 class Dubins2dPlatformConfigValidator(BaseModel):
+    """
+    Config validator for the Dubins 2D platform.
+
+    Attributes
+    ----------
+    position : list of floats
+    speed : float
+    heading : float
+    """
     position: typing.List[float]
     speed: float
     heading: float
 
     @validator("position")
     def check_position_len(cls, v, field):
+        """
+        Ensures that all fields are of a certain length - (2,).
+        """
         check_len = 2
         if len(v) != check_len:
             raise ValueError(f"{field.name} provided to DubinsPlatformConfigValidator is not length {check_len}")
@@ -23,21 +38,53 @@ class Dubins2dPlatformConfigValidator(BaseModel):
 
 
 class Dubins2dSimulatorResetValidator(BaseSimulatorResetValidator):
+    """
+    Validator for the SimulatorReset operation for Dubins2D
+    """
     agent_initialization: typing.Optional[typing.Dict[str, Dubins2dPlatformConfigValidator]] = {
         "blue0": Dubins2dPlatformConfigValidator(position=[0, 1], speed=100, heading=0)
     }
 
 
 class Dubins2dSimulator(SafeRLSimulator):
+    """
+    A class that contains all essential components of a Dubins2D simulation
+    """
 
     @classmethod
     def get_reset_validator(cls):
+        """
+        Class Method. Retrieves the Dubin2d reset validator
+
+        Return
+        ------
+        Dubins2dSimulatorResetValidator: BaseSimulatorResetValidator
+            validator for the reset operation of the Dubins2D simulator
+
+        """
         return Dubins2dSimulatorResetValidator
 
     def get_sim_entities(self):
+        """
+        Retrieve the entities present in the simulation
+
+        Returns
+        -------
+        dict
+            dictionary of all the entities by name mapped to their object references.
+        """
+
         return {agent_id: bp.Dubins2dPlatform(name=agent_id) for agent_id in self.config.agent_configs.keys()}
 
     def get_platforms(self):
+        """
+        Retrieve the platform present in the simulation
+
+        Returns
+        -------
+        tuple
+            tuple of all platform alongside their name , reference and config
+        """
         sim_platforms = tuple(
             Dubins2dPlatform(platform_name=agent_id, platform=entity, platform_config=self.config.agent_configs[agent_id].platform_config)
             for agent_id,
@@ -46,6 +93,15 @@ class Dubins2dSimulator(SafeRLSimulator):
         return sim_platforms
 
     def reset_sim_entities(self, config):
+        """
+        Reset the internal states of all the simulation entities.
+
+        Params
+        ------
+        config : dict
+            contains configuration properties
+        """
+
         config = self.get_reset_validator()(**config)
         for agent_id, entity in self.sim_entities.items():
             init_params = config.agent_initialization[agent_id]
@@ -57,6 +113,17 @@ class Dubins2dSimulator(SafeRLSimulator):
 
 
 class Dubins3dPlatformConfigValidator(BaseModel):
+    """
+    Config validator for the Dubins 3D platform.
+
+    Attributes
+    ----------
+    position : list of floats
+    speed : float
+    heading : float
+    gamma: float
+    roll : float
+    """
     position: typing.List[float]
     speed: float
     heading: float
@@ -65,6 +132,9 @@ class Dubins3dPlatformConfigValidator(BaseModel):
 
     @validator("position")
     def check_position_len(cls, v, field):
+        """
+        Ensures that all fields are of a certain length - (3,).
+        """
         check_len = 3
         if len(v) != check_len:
             raise ValueError(f"{field.name} provided to Dubins3dPlatformConfigValidator is not length {check_len}")
@@ -72,21 +142,52 @@ class Dubins3dPlatformConfigValidator(BaseModel):
 
 
 class Dubins3dSimulatorResetValidator(BaseSimulatorResetValidator):
+    """
+    Validator for the SimulatorReset operation for Dubins3D
+    """
     agent_initialization: typing.Optional[typing.Dict[str, Dubins3dPlatformConfigValidator]] = {
         "blue0": Dubins3dPlatformConfigValidator(position=[0, 1, 2], speed=100, heading=0, gamma=0, roll=0)
     }
 
 
 class Dubins3dSimulator(SafeRLSimulator):
+    """
+    A class that contains all essential components of a Dubins 3D simulation
+    """
 
     @classmethod
     def get_reset_validator(cls):
+        """
+        Class Method. Retrieves the Dubin 3D reset validator
+
+        Return
+        ------
+        Dubins3DSimulatorResetValidator: BaseSimulatorResetValidator
+            validator for the reset operation of the Dubins2D simulator
+
+        """
         return Dubins3dSimulatorResetValidator
 
     def get_sim_entities(self):
+        """
+        Retrieve the entities present in the simulation
+
+        Returns
+        -------
+        dict
+            dictionary of all the entities by name mapped to their object references.
+        """
         return {agent_id: bp.Dubins3dPlatform(name=agent_id) for agent_id in self.config.agent_configs.keys()}
 
     def get_platforms(self):
+        """
+        Retrieve the platform present in the simulation
+
+        Returns
+        -------
+        tuple
+            tuple of all platform alongside their name , reference and config
+        """
         sim_platforms = tuple(
             Dubins3dPlatform(platform_name=agent_id, platform=entity, platform_config=self.config.agent_configs[agent_id].platform_config)
             for agent_id,
@@ -95,6 +196,15 @@ class Dubins3dSimulator(SafeRLSimulator):
         return sim_platforms
 
     def reset_sim_entities(self, config):
+        """
+        Reset the internal states of all the simulation entities.
+
+        Params
+        ------
+        config : dict
+            contains configuration properties
+        """
+
         config = self.get_reset_validator()(**config)
         for agent_id, entity in self.sim_entities.items():
             init_params = config.agent_initialization[agent_id]
