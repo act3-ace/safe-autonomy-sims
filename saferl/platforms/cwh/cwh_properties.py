048ef6b785a288c6e1b4b2540fcefcb4061cd819
james.cunningham <james.cunningham@jacobs.com>
2021-10-21 17:14:56 -0400

    Set defaults to float and use unique property names

--- saferl/platforms/cwh/cwh_properties.py
+++ saferl/platforms/cwh/cwh_properties.py
@@ -7,23 +7,23 @@ from typing_extensions import Annotated
 
 class ThrustProp(BoxProp):
     name: str = "thrust"
-    low: Annotated[typing.List[StrictFloat], Field(min_items=1, max_items=1)] = [-1]
-    high: Annotated[typing.List[StrictFloat], Field(min_items=1, max_items=1)] = [1]
+    low: Annotated[typing.List[StrictFloat], Field(min_items=1, max_items=1)] = [-1.0]
+    high: Annotated[typing.List[StrictFloat], Field(min_items=1, max_items=1)] = [1.0]
     unit: Annotated[typing.List[StrictStr], Field(min_items=1, max_items=1)] = ["newtons"]
     description: str = "Direct Thrust Control"
 
 
 class PositionProp(BoxProp):
     name: str = "position"
-    low: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [-80000] * 3
-    high: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [80000] * 3
+    low: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [-80000.0] * 3
+    high: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [80000.0] * 3
     unit: Annotated[typing.List[StrictStr], Field(min_items=3, max_items=3)] = ["m"] * 3
     description: str = "Position Sensor Properties"
 
 
 class VelocityProp(BoxProp):
     name: str = "velocity"
-    low: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [-10000] * 3
-    high: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [10000] * 3
+    low: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [-10000.0] * 3
+    high: Annotated[typing.List[StrictFloat], Field(min_items=3, max_items=3)] = [10000.0] * 3
     unit: Annotated[typing.List[StrictStr], Field(min_items=3, max_items=3)] = ["m/s"] * 3
     description: str = "Velocity Sensor Properties"
