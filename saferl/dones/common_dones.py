6763f4529e150a2faf4cd384725a679d033c7c00
John McCarroll <jtm5356@rit.edu>
2021-10-25 11:52:45 -0400

    added docs for common_dones.py

--- saferl/dones/common_dones.py
+++ saferl/dones/common_dones.py
@@ -1,22 +1,59 @@
+"""
+This module contains functions that define common terminal conditions across environments.
+"""
+
 import numpy as np
 from act3_rl_core.dones.done_func_base import DoneFuncBase, DoneFuncBaseValidator, DoneStatusCodes
 from act3_rl_core.libraries.environment_dict import DoneDict
 
 
 class TimeoutDoneValidator(DoneFuncBaseValidator):
+    """
+    This class validates that the TimeoutDoneFunction config contains the max_sim_time value.
+    """
     max_sim_time: float
 
 
 class TimeoutDoneFunction(DoneFuncBase):
+    """
+    A done function that determines if the max episode time has been reached.
+    """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     @classmethod
     def get_validator(cls):
+        """
+        Params
+        ------
+        cls : constructor function
+
+        Returns
+        -------
+        TimeoutDoneValidator
+            config validator for the TimeoutDoneValidator.
+        """
         return TimeoutDoneValidator
 
     def __call__(self, observation, action, next_observation, next_state):
+        """
+        Params
+        ------
+        observation : np.ndarray
+            np.ndarray describing the current observation
+        action : np.ndarray
+            np.ndarray describing the current action
+        next_observation : np.ndarray
+            np.ndarray describing the incoming observation
+        next_state : np.ndarray
+            np.ndarray describing the incoming state
+
+        Returns
+        -------
+            done : DoneDict
+                dictionary containing the done condition for the current agent.
+        """
 
         done = DoneDict()
 
