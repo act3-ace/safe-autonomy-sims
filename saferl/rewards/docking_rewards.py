6092ab4540f1af13ced94cc8129f04ee8bfd9798
John McCarroll <jtm5356@rit.edu>
2021-10-25 11:26:09 -0400

    copied saferl docking rewards

--- saferl/rewards/docking_rewards.py
+++ saferl/rewards/docking_rewards.py
@@ -12,7 +12,7 @@ from numpy_ringbuffer import RingBuffer
 
 class CWHDistanceChangeRewardValidator(RewardFuncBaseValidator):
     """
-    scale: Scalor value to adjust magnitude of the reward
+    scale: Scalar value to adjust magnitude of the reward
     """
 
     scale: float
@@ -86,3 +86,131 @@ class CWHDistanceChangeReward(RewardFuncBase):
         reward[self.config.agent_name] = val
 
         return reward
+
+
+########################################################################################################################
+class TimeRewardValidator(RewardFuncBaseValidator):
+    """
+    scale: Scalar value to adjust magnitude of the reward
+    """
+    scale: float
+
+
+class TimeRewardFunction(RewardFuncBase):
+    def __init__(self, name=None, reward=None):
+        super().__init__(name=name, reward=reward)
+
+    @classmethod
+    def get_validator(cls):
+        """
+        Method to return class's Validator.
+        """
+        return TimeRewardValidator
+
+    def __call__(
+            self,
+            observation: OrderedDict,
+            action,
+            next_observation: OrderedDict,
+            state: StateDict,
+            next_state: StateDict,
+            observation_space: StateDict,
+            observation_units: StateDict,
+    ) -> RewardDict:
+
+
+
+
+    def reset(self, sim_state):
+        super().reset(sim_state)
+        self.previous_step_size = 0
+
+    def _increment(self, sim_state, step_size):
+        # update state
+        self.previous_step_size = step_size
+
+    def _process(self, sim_state):
+        step_reward = self.previous_step_size * self.reward
+        return step_reward
+
+
+
+
+
+
+class DistanceChangeRewardProcessor(RewardProcessor):
+    def __init__(self, name=None, deputy=None, docking_region=None, reward=None):
+        super().__init__(name=name, reward=reward)
+        self.deputy = deputy
+        self.docking_region = docking_region
+
+    def reset(self, sim_state):
+        super().reset(sim_state)
+        self.cur_distance = distance(sim_state.env_objs[self.deputy], sim_state.env_objs[self.docking_region])
+        self.prev_distance = distance(sim_state.env_objs[self.deputy], sim_state.env_objs[self.docking_region])
+
+    def _increment(self, sim_state, step_size):
+        self.prev_distance = self.cur_distance
+        self.cur_distance = distance(sim_state.env_objs[self.deputy], sim_state.env_objs[self.docking_region])
+
+    def _process(self, sim_state):
+        dist_change = self.cur_distance - self.prev_distance
+        step_reward = dist_change * self.reward
+        return step_reward
+
+
+class DistanceChangeZRewardProcessor(RewardProcessor):
+    def __init__(self, name=None, deputy=None, docking_region=None, reward=None):
+        super().__init__(name=name, reward=reward)
+        self.deputy = deputy
+        self.docking_region = docking_region
+
+    def reset(self, sim_state):
+        super().reset(sim_state)
+        self.prev_z_distance = 0
+        self.cur_z_distance = abs(sim_state.env_objs[self.deputy].z - sim_state.env_objs[self.docking_region].z)
+
+    def _increment(self, sim_state, step_size):
+        self.prev_z_distance = self.cur_z_distance
+        self.cur_z_distance = abs(sim_state.env_objs[self.deputy].z - sim_state.env_objs[self.docking_region].z)
+
+    def _process(self, sim_state):
+        dist_z_change = self.cur_z_distance - self.prev_z_distance
+        step_reward = dist_z_change * self.reward
+        return step_reward
+
+
+class SuccessRewardProcessor(RewardProcessor):
+    def __init__(self, name=None, success_status=None, reward=None, timeout=None):
+        super().__init__(name=name, reward=reward)
+        self.success_status = success_status
+        self.timeout = timeout
+
+    def _increment(self, sim_state, step_size):
+        # reward derived straight from status dict, therefore no state machine necessary
+        pass
+
+    def _process(self, sim_state):
+        step_reward = 0
+        if sim_state.status[self.success_status]:
+            step_reward = self.reward
+            if self.timeout is not None:
+                step_reward += 1 - (sim_state.time_elapsed / self.timeout)
+        return step_reward
+
+
+class FailureRewardProcessor(RewardProcessor):
+    def __init__(self, name=None, failure_status=None, reward=None):
+        super().__init__(name=name, reward=reward)
+        self.failure_status = failure_status
+
+    def _increment(self, sim_state, step_size):
+        # reward derived straight from status dict, therefore no state machine necessary
+        pass
+
+    def _process(self, sim_state):
+        step_reward = 0
+        if sim_state.status[self.failure_status]:
+            step_reward = self.reward[sim_state.status[self.failure_status]]
+        return step_reward
+
