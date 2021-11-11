79edd460afbc9a22a303bed359e026d0f007420d
John McCarroll <jtm5356@rit.edu>
2021-11-11 15:42:44 -0500

    created evaluate helper function

--- test/test_simulators/test_backend_simulators/conftest.py
+++ test/test_simulators/test_backend_simulators/conftest.py
@@ -3,8 +3,9 @@ This module holds fixtures common to all backend simulator tests.
 
 Author: John McCarroll
 """
+
 import pytest
-# import numpy as np
+import numpy as np
 
 
 @pytest.fixture
@@ -25,30 +26,34 @@ def acted_entity(num_steps, entity, action):
     return entity
 
 
-# @pytest.fixture
-# def evaluate(acted_entity, attr_targets, error_bound):
-#     # evaluation
-#     if error_bound is None:
-#         # direct comparison
-#         for key, value in attr_targets.items():
-#             if type(value) in [np.ndarray, list]:
-#                 # handle array case
-#                 return np.array_equal(entity.__getattribute__(key), value), \
-#                     "Resulting attribute values different from expected: {}".format(value)
-#             else:
-#                 # compare entity value and target value
-#                 return entity.__getattribute__(key) == value, \
-#                     "Resulting attribute value different from expected: {}".format(value)
-#     else:
-#         # bounded comparison
-#         for key, value in attr_targets.items():
-#             if type(value) in [np.ndarray, list]:
-#                 # handle array case
-#                 value = np.array(value)
-#                 in_bounds = value <= error_bound
-#                 return np.all(in_bounds), \
-#                     "Resulting attribute values significantly different from expected: {}".format(value)
-#             else:
-#                 # compare entity value and target value
-#                 return abs(entity.__getattribute__(key) - value) <= error_bound, \
-#                     "Resulting attribute value significantly different from expected: {}".format(value)
+def evaluate(entity, attr_targets, error_bound=None):
+    # evaluation
+    if error_bound is None:
+        # direct comparison
+        for key, value in attr_targets.items():
+            if type(value) in [np.ndarray, list]:
+                # handle array case
+                assert np.array_equal(entity.__getattribute__(key), value), \
+                    "Expected attribute {} values to be {} but instead received {}".format(
+                        key, value, entity.__getattribute__(key))
+            else:
+                # compare entity value and target value
+                assert entity.__getattribute__(key) == value, \
+                    "Expected attribute {} value(s) to be {} but instead received {}".format(
+                        key, value, entity.__getattribute__(key))
+    else:
+        # bounded comparison
+        for key, value in attr_targets.items():
+            if type(value) in [np.ndarray, list]:
+                # handle array case
+                value = np.array(value)
+                np.abs(np.subtract(entity.__getattribute__(key), value))
+                in_bounds = value <= error_bound
+                assert np.all(in_bounds), \
+                    "Expected attribute {} values to be {} +/- {} but instead received {}".format(
+                        key, value, error_bound, entity.__getattribute__(key))
+            else:
+                # compare entity value and target value
+                assert abs(entity.__getattribute__(key) - value) <= error_bound, \
+                    "Expected attribute {} value to be {} +/- {} but instead received {}".format(
+                        key, value, error_bound, entity.__getattribute__(key))
