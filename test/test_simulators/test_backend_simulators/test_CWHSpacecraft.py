2ffccde9e9b6e5d6cba1a4bb5551a3f4cac45c66
John McCarroll <jtm5356@rit.edu>
2021-11-11 15:42:44 -0500

    created evaluate helper function

--- test/test_simulators/test_backend_simulators/test_CWHSpacecraft.py
+++ test/test_simulators/test_backend_simulators/test_CWHSpacecraft.py
@@ -8,6 +8,7 @@ import pytest
 import numpy as np
 
 from saferl_sim.cwh.cwh import CWHSpacecraft
+from test.test_simulators.test_backend_simulators.conftest import evaluate
 
 
 # override entity fixture
@@ -34,29 +35,5 @@ test_configs = [
 
 
 @pytest.mark.parametrize("initial_entity_state,action,num_steps,attr_targets,error_bound", test_configs, indirect=True)
-def test_CWHSpacecraft(initial_entity_state, action, num_steps, attr_targets, error_bound):
-    # evaluation
-    if error_bound is None:
-        # direct comparison
-        for key, value in attr_targets.items():
-            if type(value) in [np.ndarray, list]:
-                # handle array case
-                assert np.array_equal(entity.__getattribute__(key), value), \
-                    "Resulting attribute values different from expected: {}".format(value)
-            else:
-                # compare entity value and target value
-                assert entity.__getattribute__(key) == value, \
-                    "Resulting attribute value different from expected: {}".format(value)
-    else:
-        # bounded comparison
-        for key, value in attr_targets.items():
-            if type(value) in [np.ndarray, list]:
-                # handle array case
-                value = np.array(value)
-                in_bounds = value <= error_bound
-                assert np.all(in_bounds), \
-                    "Resulting attribute values significantly different from expected: {}".format(value)
-            else:
-                # compare entity value and target value
-                assert abs(entity.__getattribute__(key) - value) <= error_bound, \
-                    "Resulting attribute value significantly different from expected: {}".format(value)
+def test_CWHSpacecraft(entity, action, num_steps, attr_targets, error_bound):
+    evaluate(entity, attr_targets, error_bound=error_bound)
