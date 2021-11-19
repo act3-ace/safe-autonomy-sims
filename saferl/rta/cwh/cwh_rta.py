9aad5569a91ba9301e996e32f44abe0f7871dfc8
Umberto Ravaioli <uravaioli@toyon.com>
2021-11-18 20:09:07 -0800

    tech sprint cwh cleanup

--- saferl/rta/cwh/cwh_rta.py
+++ saferl/rta/cwh/cwh_rta.py
@@ -5,7 +5,9 @@ from saferl.rta.base_rta import ConstraintModule, ExplicitSimplexModule
 
 class DockingRTA(ExplicitSimplexModule):
 
-    def __init__(self, x_vel_limit=10, y_vel_limit=10, T_backup=5, Nskip=1, N_checkall=5, v0=0.2, v1_coef=2, kappa=1, n=0.001027, **kwargs):
+    def __init__(
+        self, x_vel_limit=10, y_vel_limit=10, T_backup=5, Nskip=1, N_checkall=5, v0=0.2, v1_coef=2, kappa=1, n=0.001027, m=12, **kwargs
+    ):
         super().__init__(**kwargs)
         self.T_backup = T_backup
         self.Nskip = Nskip
@@ -22,17 +24,21 @@ class DockingRTA(ExplicitSimplexModule):
         self.n = n
         self.v1 = self.v1_coef * self.n
 
+        self.m = m
+
+        self.A, self.B = self._gen_dynamics_matrices()
+
     def _setup_constraints(self):
         # TODO: Automate this?
-        self.constraint_rel_vel = Constraint_rel_vel()
-        self.constraint_x_vel = Constraint_x_vel()
-        self.constraint_y_vel = Constraint_y_vel()
+        self.constraint_rel_vel = Constraint_rel_vel(v0=self.v0, v1=self.v1)
+        self.constraint_x_vel = ConstraintStateLimit(limit_val=self.x_vel_limit, state_index=2)
+        self.constraint_y_vel = ConstraintStateLimit(limit_val=self.y_vel_limit, state_index=3)
         return ['constraint_rel_vel', 'constraint_x_vel', 'constraint_y_vel']
 
     def _backup_control(self, action, observation):
         state_vec = self._get_state_vector(observation)
 
-        pred_state = self._pred_state_vector(sim_state, step_size, control)
+        pred_state = self._pred_state_vector(action, state_vec, self.config.step_size)
 
         if self.constraint_x_vel.h_x(pred_state) < 0:
             pred_state[2] = self.x_vel_limit * np.sign(pred_state[2])
@@ -43,20 +49,59 @@ class DockingRTA(ExplicitSimplexModule):
             vH = np.linalg.norm(pred_state[2:4])
             vH_max = self.v1 * rH + self.v0
             pred_state[2:4] = pred_state[2:4] / vH * vH_max
-        accel = (pred_state[2:4] - state_vec[2:4]) / step_size
+        accel = (pred_state[2:4] - state_vec[2:4]) / self.config.step_size
 
         backup_action = (accel[0:2] - self.A[2:4] @ pred_state) * self.m
 
-        return self.clip(backup_action)
+        backup_action = self.clip(backup_action)
+
+        return backup_action
+
+    def _gen_dynamics_matrices(self):
+        m = self.m
+        n = self.n
+
+        A = np.array(
+            [
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 1],
+                [3 * n**2, 0, 0, 0, 2 * n, 0],
+                [0, 0, 0, -2 * n, 0, 0],
+                [0, 0, -(n**2), 0, 0, 0],
+            ],
+            dtype=np.float32,
+        )
+
+        B = np.array(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [1 / m, 0, 0],
+                [0, 1 / m, 0],
+                [0, 0, 1 / m],
+            ],
+            dtype=np.float32,
+        )
+
+        return A, B
+
+    def _pred_state_vector(self, action, state_vec, step_size):
+        action_vec = 1  # TODO
+
+        next_state = self.A @ state_vec + self.B @ action_vec
+        return next_state
 
 
 class Constraint_rel_vel(ConstraintModule):
 
-    def __init__(self):
-        self.setup_params()
+    def __init__(self, v0, v1):
+        self.v0 = v0
+        self.v1 = v1
 
     def h_x(self, state_vec):
-        return self.v0 + self.v1 * np.linalg.norm(state_vec[0:2]) - np.linalg.norm(state_vec[2:4])
+        return self.v0 + self.v1 * (np.linalg.norm(state_vec[0:2]) - np.linalg.norm(state_vec[2:4]))
 
     def grad(self, state_vec):
         Hs = np.array([[2 * self.v1**2, 0, 0, 0], [0, 2 * self.v1**2, 0, 0], [0, 0, -2, 0], [0, 0, 0, -2]])
@@ -70,33 +115,18 @@ class Constraint_rel_vel(ConstraintModule):
         return 0.05 * x + 0.1 * x**3
 
 
-class Constraint_x_vel(ConstraintModule):
+class ConstraintStateLimit(ConstraintModule):
 
-    def __init__(self):
-        self.setup_params()
-
-    def h_x(self, state_vec):
-        return self.x_vel_limit**2 - state_vec[2]**2
-
-    def grad(self, state_vec):
-        gh = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, -2, 0], [0, 0, 0, 0]])
-        g = gh @ state_vec
-        return g
-
-    def alpha(self, x):
-        return 0.0005 * x + 0.001 * x**3
-
-
-class Constraint_y_vel(ConstraintModule):
-
-    def __init__(self):
-        self.setup_params()
+    def __init__(self, limit_val, state_index):
+        self.limit_val = limit_val
+        self.state_index = state_index
 
     def h_x(self, state_vec):
-        return self.y_vel_limit**2 - state_vec[3]**2
+        return self.limit_val**2 - state_vec[self.state_index]**2
 
     def grad(self, state_vec):
-        gh = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, -2]])
+        gh = np.zeros((state_vec.size, state_vec.size), dtype=float)
+        gh[self.state_index, self.state_index] = -2
         g = gh @ state_vec
         return g
 
