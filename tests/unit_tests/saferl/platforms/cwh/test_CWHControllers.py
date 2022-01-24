c3b94ec1843ca8eb9454fc34bd90d8e56b33ee27
vgangal <Vardaan.Gangal@Jacobs.com>
2022-01-24 12:56:24 -0500

    WIP tests for CWHControllers

--- tests/unit_tests/saferl/platforms/cwh/test_CWHControllers.py
+++ tests/unit_tests/saferl/platforms/cwh/test_CWHControllers.py
@@ -1,9 +1,11 @@
-import pytest
 from unittest import mock
+
 import numpy as np
-from saferl_sim.cwh.cwh import CWHSpacecraft
+import pytest
+
+from saferl.platforms.cwh.cwh_controllers import CWHController, ThrustController, ThrustControllerValidator
 from saferl.platforms.cwh.cwh_platform import CWHPlatform
-from saferl.platforms.cwh.cwh_controllers import CWHController, ThrustControllerValidator, ThrustController
+from saferl_sim.cwh.cwh import CWHSpacecraft
 
 # how would you test CWHController ?
 
@@ -14,24 +16,22 @@ def setup_arb_CWHPlatform():
     platform_config = {}
     # below should be a saferl_sim.cwh.cwh.CWHSpacecraft
     platform_obj = CWHSpacecraft()
-    platform = CWHPlatform(platform_name,platform_obj,platform_config)
+    platform = CWHPlatform(platform_name, platform_obj, platform_config)
     return platform
 
 
 @pytest.fixture(name='env_config')
 def setup_env_config():
-    config = ("saferl.platforms.cwh.cwh_controllers.ThrustController", {
-                        "name": "X Thrust", "axis": 0 })
+    config = ("saferl.platforms.cwh.cwh_controllers.ThrustController", {"name": "X Thrust", "axis": 0})
 
     return config
+
 
 """
 mock the cwh_props, into a random prop
 mock CWHPlatform into a arbitrary obj for the platform
 mock
 """
-
-
 
 
 # currently failing - need appropriate args to constructor
@@ -42,19 +42,14 @@ def test_CWHController_applycontrol():
     #config = {}
     #control_properties =
     mock_platform = mock.MagicMock()
-    mock_config = {'name':'blue0'}
+    mock_config = {'name': 'blue0'}
     mock_control_props = mock.MagicMock()
 
-
     obj = CWHController()
-    dummy_np_arr = np.array([0.,0.,0.])
+    dummy_np_arr = np.array([0., 0., 0.])
 
     with pytest.raises(NotImplementedError) as excinfo:
         obj.apply_control(dummy_np_arr)
-
-
-
-
 
 
 """
@@ -62,7 +57,6 @@ Test the following : CWHController, ThrustControllerValidator, ThrustController
 
 A little unsure of how to test CWHController
 """
-
 """
 Tests for CWHController
 
@@ -73,8 +67,6 @@ control_properties= <class 'saferl.platforms.cwh.cwh_properties.ThrustProp'>
 
 
 """
-
-
 """
 # currently failing - need appropriate args to constructor
 def test_CWHController_applycontrol(env_config):
