ae382cc472c9bf7af820d4a7262bdf3bb6ed33ac
james.cunningham <james.cunningham@jacobs.com>
2022-01-25 19:11:05 -0500

    Redo test stubs

--- tests/factories/dubins/dubins_platform.py
+++ tests/factories/dubins/dubins_platform.py
@@ -1,7 +1,7 @@
 import factory
 
 import saferl.platforms.dubins.dubins_platform as p
-from tests.factories.dubins.dubins_entity import Dubins2dAircraftFactory
+from tests.factories.dubins.dubins_entity import Dubins2dAircraftFactory, Dubins3dAircraftFactory
 from tests.factories.base_factories.platform import BasePlatformFactory
 import saferl.platforms.dubins.dubins_sensors as s
 import saferl.platforms.dubins.dubins_controllers as c
@@ -9,12 +9,30 @@ import saferl.platforms.dubins.dubins_controllers as c
 
 class Dubins2dPlatformFactory(BasePlatformFactory):
     class Meta:
-        model = p.DubinsPlatform
+        model = p.Dubins2dPlatform
 
     platform = factory.SubFactory(Dubins2dAircraftFactory)
     name = factory.Faker('first_name_nonbinary')
     parts_list = [
         (c.AccelerationController, {'axis': 0}),
         (c.YawRateController, {'axis': 1}),
+        (s.PositionSensor, {}),
+        (s.VelocitySensor, {}),
+        (s.HeadingSensor, {})
+    ]
 
+
+class Dubins3dPlatformFactory(BasePlatformFactory):
+    class Meta:
+        model = p.Dubins3dPlatform
+
+    platform = factory.SubFactory(Dubins3dAircraftFactory)
+    name = factory.Faker('first_name_nonbinary')
+    parts_list = [
+        (c.AccelerationController, {'axis': 0}),
+        (c.YawRateController, {'axis': 1}),
+        (s.PositionSensor, {}),
+        (s.VelocitySensor, {}),
+        (s.HeadingSensor, {}),
+        (s.FlightPathSensor, {}),
     ]
