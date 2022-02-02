d73645a6eed09289b2b0724abc69accceebd6f65
vgangal <Vardaan.Gangal@Jacobs.com>
2022-02-02 15:17:12 -0500

    docstrings

--- tests/unit_tests/saferl/platforms/cwh/test_CWHPlatform.py
+++ tests/unit_tests/saferl/platforms/cwh/test_CWHPlatform.py
@@ -1,3 +1,7 @@
+"""
+Tests for the CWHPlatform module
+"""
+
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -13,6 +17,11 @@ Tests for get_applied_action
 
 @pytest.mark.unit_test
 def test_CWHPlatform_getAppliedAction_default(cwh_platform):
+    """
+    Test for CWHPlatform - get_applied_action method,
+    testing to see whether it returns default values
+    """
+
     platform_obj = cwh_platform
     expected = np.array([0., 0., 0.])
     assert np.array_equiv(platform_obj.get_applied_action(), expected)
@@ -23,12 +32,19 @@ get_applied_tests = [(np.array([15., 16., 17.])), (np.array([100., 100., 100.]))
 
 @pytest.fixture(name='applied_action')
 def applied_action(request):
+    """
+    fixture to obtain the parameter 'applied_action' from a list
+    """
     return request.param
 
 
 @pytest.mark.unit_test
 @pytest.mark.parametrize('applied_action', get_applied_tests, indirect=True)
 def test_CWHPlatform_getAppliedAction(cwh_platform, applied_action):
+    """
+    Test CWHPlatform - get_applied_action() method
+    This is a parametrized test with 'get_applied_tests' being the test cases
+    """
     cwh_platform._last_applied_action = applied_action
 
     result = cwh_platform.get_applied_action()
@@ -45,6 +61,10 @@ action_to_platform_tests = [(np.array([5., 6., 7.])), (np.array([10., 10., 10.])
 @pytest.mark.unit_test
 @pytest.mark.parametrize('applied_action', action_to_platform_tests, indirect=True)
 def test_CWHPlatform_saveActionToPlatform(cwh_platform, applied_action):
+    """
+    Tests for CWHPlatform method - save_action_to_platform(),
+    This is a parametrized test, where the tests are in the action_to_platform_tests
+    """
     cwh_platform.save_action_to_platform(applied_action[0], 0)
     cwh_platform.save_action_to_platform(applied_action[1], 1)
     cwh_platform.save_action_to_platform(applied_action[2], 2)
@@ -67,6 +87,9 @@ pos_attr_tests = [
 @pytest.mark.unit_test
 @pytest.mark.parametrize("pos_input,pos_expected", pos_attr_tests, indirect=True)
 def test_position_attribute(cwh_platform_pos, pos_expected):
+    """
+    Test CWHPlatform - position property
+    """
     assert np.array_equiv(cwh_platform_pos.position, pos_expected)
 
 
@@ -84,6 +107,9 @@ vel_attr_tests = [
 @pytest.mark.unit_test
 @pytest.mark.parametrize("vel_input,vel_expected", vel_attr_tests, indirect=True)
 def test_velocity_attrbute(cwh_platform_vel, vel_expected):
+    """
+    Test for velocity property getter method
+    """
     calced = cwh_platform_vel.velocity
     assert np.array_equiv(calced, vel_expected)
 
@@ -95,17 +121,26 @@ Tests for sim_time attribute , accessor and mutator
 
 @pytest.mark.unit_test
 def test_sim_time_default(cwh_platform):
+    """
+    Test for sim_time , default case , must be equal to 0.0
+    """
     simulation_time = cwh_platform.sim_time
     assert simulation_time == 0.0
 
 
 @pytest.fixture(name='set_to_time')
 def setup_set_to_time(request):
+    """
+    fixture to obtain sim_time parameter from a parameter list
+    """
     return request.param
 
 
 @pytest.fixture(name='simtime_platform')
 def setup_sim_time_platform(cwh_platform, set_to_time):
+    """
+    fixture to set sim_time with respect to the cwh_platform
+    """
     cwh_platform._sim_time = set_to_time
     return cwh_platform
 
@@ -116,6 +151,9 @@ time_prop_tests = [(10.5), (100.), (200.)]
 @pytest.mark.unit_test
 @pytest.mark.parametrize("set_to_time", time_prop_tests, indirect=True)
 def test_simtime_property(simtime_platform, set_to_time):
+    """
+    parametrized test for to test if sim_time property getter method
+    """
     assert simtime_platform.sim_time == set_to_time
 
 
@@ -125,6 +163,9 @@ time_accsr_tests = [(10.5), (100.), (200.)]
 @pytest.mark.unit_test
 @pytest.mark.parametrize("set_to_time", time_accsr_tests, indirect=True)
 def test_simtime_setter(cwh_platform, set_to_time):
+    """
+    parametrized test for to test if sim_time property setter method
+    """
     cwh_platform.sim_time = set_to_time
     assert cwh_platform._sim_time == set_to_time
 
@@ -136,4 +177,7 @@ Tests for 'operable' property
 
 @pytest.mark.unit_test
 def test_is_operable(cwh_platform):
-    assert cwh_platform.operable == True
+    """
+    validate that cwh_platform is always operable
+    """
+    assert cwh_platform.operable is True
