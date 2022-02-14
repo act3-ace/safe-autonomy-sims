b35c1cd11a0134b1f18f0e3ea9f734c8e64d4834
John McCarroll <jtm5356@rit.edu>
2022-02-14 11:00:24 -0500

    formatting

---
+++ tests/unit_tests/saferl/platforms/dubins/dubins_controllers/test_CombinedPitchRollAccelerationController.py
@@ -0,0 +1,57 @@
+"""
+Tests for the CombinedPitchRollAccelerationController module of the dubins platform.
+
+Author: John McCarroll
+"""
+
+from unittest import mock
+
+import numpy as np
+import pytest
+
+from saferl.platforms.dubins.dubins_controllers import CombinedPitchRollAccelerationController
+
+
+@pytest.fixture(name="controller")
+def setup_controller(dubins_platform, config):
+    """
+    Set up CombinedPitchRollAccelerationController with default values
+    """
+
+    return CombinedPitchRollAccelerationController(parent_platform=dubins_platform, config=config)
+
+
+# Unit Tests
+@pytest.mark.unit_test
+def test_constructor(config, control_properties):
+    """
+    Simple test to ensure construction and attribute assignment functions appropriately
+    """
+    parent_platform = mock.MagicMock()
+
+    cut = CombinedPitchRollAccelerationController(parent_platform=parent_platform, config=config)
+
+    assert cut.config.axis == config.get("axis")
+    assert cut._properties == control_properties()  # pylint: disable=W0212
+    assert cut._parent_platform == parent_platform  # pylint: disable=W0212
+
+
+@pytest.mark.unit_test
+def test_apply_control(controller, control):
+    """
+    Simple test for the apply_control method of CombinedPitchRollAccelerationController
+    """
+    controller.apply_control(control)
+    assert controller.parent_platform._last_applied_action == control  # pylint: disable=W0212
+
+
+@pytest.mark.unit_test
+def test_get_applied_action(controller):
+    """
+    Test for the get_applied_control method of CombinedPitchRollAccelerationController
+    """
+
+    dummy_arr = np.array([10., 20., 30.])
+    controller.parent_platform._last_applied_action = dummy_arr  # pylint: disable=W0212
+
+    assert np.array_equal(controller.get_applied_control(), dummy_arr)  # pylint: disable=W0212
--- tests/unit_tests/saferl/platforms/dubins/dubins_controllers/test_RateController.py
+++ tests/unit_tests/saferl/platforms/dubins/dubins_controllers/test_RateController.py
@@ -10,48 +10,6 @@ import numpy as np
 import pytest
 
 from saferl.platforms.dubins.dubins_controllers import RateController
-from saferl.platforms.dubins.dubins_platform import Dubins3dPlatform
-
-
-@pytest.fixture(name="control")
-def get_control():
-    """
-    Return control value
-    """
-
-    return 10
-
-
-@pytest.fixture(name="config")
-def get_config():
-    """
-    Setup config dict
-    """
-
-    axis = 2
-    return {"axis": axis}
-
-
-@pytest.fixture(name="control_properties")
-def get_control_properties():
-    """
-    Return a mock of control_properties
-    """
-    return mock.MagicMock(return_value="props")
-
-
-@pytest.fixture(name='dubins_platform')
-def setup_dubins_platform():
-    """
-    Set up basic Dubins3dPlatform, with default values
-    """
-
-    platform_name = 'friendly_platform'
-    platform_config = []
-    aircraft = mock.MagicMock()
-
-    platform_obj = Dubins3dPlatform(platform_name, aircraft, platform_config)
-    return platform_obj
 
 
 @pytest.fixture(name="rate_controller")
