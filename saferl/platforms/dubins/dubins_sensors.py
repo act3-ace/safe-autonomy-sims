7cbc61bdd0f834d756bc9efd2214b00b344688d8
justin <justin.merrick.1@us.af.mil>
2022-02-22 14:43:16 -0500

    feat: roll and orientation as quaternion

--- saferl/platforms/dubins/dubins_sensors.py
+++ saferl/platforms/dubins/dubins_sensors.py
@@ -166,7 +166,86 @@ class FlightPathSensor(DubinsSensor):
 
 
 PluginLibrary.AddClassToGroup(
+    HeadingSensor, "Sensor_Flight_Path_Angle", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+PluginLibrary.AddClassToGroup(
     FlightPathSensor, "Sensor_Flight_Path_Angle", {
+        "simulator": Dubins3dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS3D
+    }
+)
+
+
+class RollSensor(DubinsSensor):
+    """
+    Implementation of a sensor to give roll angle at any time.
+    """
+
+    def __init__(self, parent_platform, config, measurement_properties=dubins_props.RollProp):
+        super().__init__(measurement_properties=measurement_properties, parent_platform=parent_platform, config=config)
+
+    def _calculate_measurement(self, state):
+        """
+        Calculate the measurement - roll angle
+
+        Params
+        ------
+        state: np.ndarray
+            current state
+
+        Returns
+        -------
+        float
+            roll angle
+        """
+        return np.array([np.deg2rad(self.parent_platform.roll)], dtype=np.float32)
+
+
+PluginLibrary.AddClassToGroup(
+    HeadingSensor, "Sensor_Roll", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+PluginLibrary.AddClassToGroup(
+    FlightPathSensor, "Sensor_Roll", {
+        "simulator": Dubins3dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS3D
+    }
+)
+
+
+class QuaternionSensor(DubinsSensor):
+    """
+    Implementation of a sensor to give a quaternion view of the orientation at any time.
+    """
+
+    def __init__(self, parent_platform, config, measurement_properties=dubins_props.QuaternionProp):
+        super().__init__(measurement_properties=measurement_properties, parent_platform=parent_platform, config=config)
+
+    def _calculate_measurement(self, state):
+        """
+        Calculate the measurement - quaternion
+
+        Params
+        ------
+        state: np.ndarray
+            current state
+
+        Returns
+        -------
+        np.ndarray
+            quaternion
+        """
+        return self.parent_platform.orientation.as_quat()
+
+
+PluginLibrary.AddClassToGroup(
+    HeadingSensor, "Sensor_Orientation", {
+        "simulator": Dubins2dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS2D
+    }
+)
+PluginLibrary.AddClassToGroup(
+    FlightPathSensor, "Sensor_Orientation", {
         "simulator": Dubins3dSimulator, "platform_type": DubinsAvailablePlatformTypes.DUBINS3D
     }
 )
