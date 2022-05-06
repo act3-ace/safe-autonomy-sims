69be2c1596f421b7fabe9f5d0ac452b0815b9d46
John McCarroll <jtm5356@rit.edu>
2022-05-06 12:41:10 -0400

    added boilerplates

--- saferl/core/dones/common_dones.py
+++ saferl/core/dones/common_dones.py
@@ -1,4 +1,14 @@
 """
+# -------------------------------------------------------------------------------
+# Air Force Research Laboratory (AFRL) Autonomous Capabilities Team (ACT3)
+# Reinforcement Learning Core (CoRL) Runtime Assurance Extensions
+#
+# This is a US Government Work not subject to copyright protection in the US.
+#
+# The use, dissemination or disclosure of data in this file is subject to
+# limitation or restriction. See accompanying README and LICENSE for details.
+# -------------------------------------------------------------------------------
+
 This module contains functions that define common terminal conditions across environments.
 """
 
