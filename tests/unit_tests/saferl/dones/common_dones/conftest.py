8d6b26d6f3cc4ff3fe8b9a35bf274e8a86dc7f93
james.cunningham <james.cunningham@jacobs.com>
2022-05-18 23:50:45 -0400

    tests: fix patch path

--- tests/unit_tests/saferl/dones/common_dones/conftest.py
+++ tests/unit_tests/saferl/dones/common_dones/conftest.py
@@ -43,7 +43,7 @@ def call_results(cut, observation, action, next_observation, next_state, platfor
     results : DoneDict
         The resulting DoneDict from calling the DockingVelocityLimitDoneFunction
     """
-    with mock.patch("saferl.core.dones.common_dones.get_platform_by_name") as func:
+    with mock.patch("saferl.dones.common_dones.get_platform_by_name") as func:
         func.return_value = platform
         results = cut(observation, action, next_observation, next_state)
         return results
