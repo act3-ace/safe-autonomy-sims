1b19d6b7b98e6495fac8ac101c1ab32007689961
Jamie Cunningham <james.cunningham@jacobs.com>
2023-04-06 18:44:01 +0000

    Revert "feat!: change package name to safe_autonomy_sims"
    This reverts merge request !268

--- tests/unit_tests/saferl/dones/cwh/common/test_MaxDistanceOriginDoneFunction.py
+++ tests/unit_tests/saferl/dones/cwh/common/test_MaxDistanceOriginDoneFunction.py
@@ -18,7 +18,7 @@ import os
 
 import pytest
 
-from safe_autonomy_sims.dones.cwh.common import MaxDistanceDoneFunction
+from saferl.dones.cwh.common import MaxDistanceDoneFunction
 from tests.conftest import delimiter, read_test_cases
 
 # Define test assay
