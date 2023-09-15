6ea049cc00c106498f830e2c84987719ddb420bb
Kochise Bennett <kcbennett@toyon.com>
2023-09-15 15:20:29 -0700

    fix rewards bug. config.platform_names not single name as is the case with dones

--- safe_autonomy_sims/rewards/cwh/inspection_rewards.py
+++ safe_autonomy_sims/rewards/cwh/inspection_rewards.py
@@ -510,8 +510,9 @@ class SafeInspectionSuccessReward(InspectionSuccessReward):
         value = reward[self.config.agent_name]
 
         if value != 0.0:
-            pos = next_state.sim_platforms[self.config.platform_name].position
-            vel = next_state.sim_platforms[self.config.platform_name].velocity
+            name = [p for p in self.config.platform_names if p in self.config.agent_name][0]
+            pos = next_state.sim_platforms[name].position
+            vel = next_state.sim_platforms[name].velocity
             state = np.concatenate((pos, vel))
             n = self.config.mean_motion
             times = np.arange(0, 2 * np.pi / n, self.config.fft_time_step)
