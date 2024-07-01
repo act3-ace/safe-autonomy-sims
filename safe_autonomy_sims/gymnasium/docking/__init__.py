d6f3759e8296adf35caab2d9f984f36f8a5707bc
Jamie Cunningham <james.cunningham@parallaxresearch.org>
2024-07-01 15:58:47 -0400

    import gym envs

---
+++ safe_autonomy_sims/gymnasium/docking/__init__.py
@@ -0,0 +1,3 @@
+from safe_autonomy_sims.gymnasium.docking.docking_v0 import DockingEnv
+
+__all__ = ['DockingEnv']
---
+++ safe_autonomy_sims/gymnasium/inspection/__init__.py
@@ -0,0 +1,6 @@
+from safe_autonomy_sims.gymnasium.inspection.inspection_v0 import InspectionEnv
+from safe_autonomy_sims.gymnasium.inspection.weighted_inspection_v0 import WeightedInspectionEnv
+from safe_autonomy_sims.gymnasium.inspection.sixdof_inspection_v0 import WeightedSixDofInspectionEnv
+
+
+__all__ = ['InspectionEnv', 'WeightedInspectionEnv', 'WeightedSixDofInspectionEnv']
